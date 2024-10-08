dab57daf1cc999732a488a04e94b70ebaa318aa6
Simooon3 <simonmesa3622@gmail.com>
2024-10-07 19:41:30 -0500

    como consumir el puerto

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,30 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const baseUrl = 'http://localhost:3000'
+
+  const getAppi = () => {
+    fetch(`${baseUrl}/pokemones`)
+    .then(response => response.json())
+    .then(data => setData(data))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Appi pokemones</h1>    
+      <button
+      onClick = {() => getAppi()}
+      >Obtener pokemones</button>
+      <ul>
+        {
+          data.map((item) => (
+            <li key = {item.id}>{item.nombre}</li>
+          ))
+        }
+      
+      </ul> 
     </>
   )
 }
